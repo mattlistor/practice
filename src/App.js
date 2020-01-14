6971680331dea0bcfc25cdc8491a40bf81a1f1aa
mattlistor <mattlistor@gmail.com>
2020-01-14 16:43:14 -0500

    here

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import duck from './duck.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component  {
+  render(){
+    return (
+      <div className="App">
+        <img src={duck} className="duck" alt="duck" />
+        <h1 className="header">Splish Splash</h1>
+        <div className="play">Play</div>
+      </div>
+    );
+  } 
 }
 
 export default App;
